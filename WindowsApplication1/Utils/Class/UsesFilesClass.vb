68a8a76536a7a8ee929e15359927e80997992f57
Fernando Rosa <fernandorosajr@gmail.com>
2020-02-11 21:43:17 -0300

    Melhoramentos e correções da função DevolverNomeDoArquivo na classe UsesFilesClass.

--- WindowsApplication1/Utils/Class/UsesFilesClass.vb
+++ WindowsApplication1/Utils/Class/UsesFilesClass.vb
@@ -1,39 +1,58 @@
-﻿Public Class UsesFilesClass
-    Public Function DevolverNomeDoArquivo(caminho As String, nomeDoArquivo As String)
+﻿Imports System.IO
+
+Public Class UsesFilesClass
+
+    Dim StringFunctions As New StringFunctionsClass
+
+    Public Function DevolverNomeDoArquivo(_nomeDoArquivoDeOrigem As String, _nomeDoArquivoDestino As String, Substituir As Boolean) As String
         Dim fileArrayList As New ArrayList
-        Dim drive As New DirectoryInfo(caminho)
-        Dim driveDestino As New DirectoryInfo(caminho & "\" & nomeDoArquivo)
+        Dim NomeDoArquivoDeOrigem As New FileInfo(_nomeDoArquivoDeOrigem)
+        Dim NomeDoArquivoDestino As New FileInfo(_nomeDoArquivoDestino)
+
+        Dim dir As DirectoryInfo
+        dir = NomeDoArquivoDestino.Directory
+
         Dim pesquisa As String
 
-        If drive.Exists = True Then
-            If driveDestino.Exists = True Then
-                fileArrayList.AddRange(drive.GetDirectories())
+        If Substituir = False Then
+            If NomeDoArquivoDestino.Exists = True Then
+                fileArrayList.AddRange(dir.GetFiles())
 
                 For x As Integer = 0 To fileArrayList.Count
-                    pesquisa = nomeDoArquivo & " (" & x + 2 & ")"
+                    Dim _nomeDoArquivoSemExtensao As String
 
-                    Dim query = From pasta As DirectoryInfo In fileArrayList
-                                Where pasta.Name.IndexOf(pesquisa) = 0
-                                Select pasta
+                    _nomeDoArquivoSemExtensao = NomeDoArquivoSemExtensao(NomeDoArquivoDestino)
+
+                    pesquisa = _nomeDoArquivoSemExtensao & " (" & x + 2 & ")"
+
+
+                    Dim query = From file As FileInfo In fileArrayList
+                                Where file.Name.IndexOf(pesquisa) = 0
+                                Select file
 
                     If query.Count = 0 Then
-                        Return pesquisa
+                        Return NomeDoArquivoDestino.DirectoryName & "\" & pesquisa & NomeDoArquivoDestino.Extension
                         Exit For
                     End If
+
                 Next
 
             Else
 
-                Return nomeDoArquivo
+                Return _nomeDoArquivoDestino
             End If
-
-        Else
-
-            'TODO: Atualizar Pasta
-            ' A Pasta Destino não existe
         End If
+
+
 
         ' https://docs.microsoft.com/pt-br/dotnet/visual-basic/programming-guide/concepts/linq/how-to-query-an-arraylist-with-linq
         ' https://docs.microsoft.com/pt-br/dotnet/visual-basic/programming-guide/language-features/strings/how-to-search-within-a-string
+    End Function
+    Function NomeDoArquivoSemExtensao(nome As FileInfo) As String
+
+        Dim _nomeDoArquivoSemExtensao As String
+        _nomeDoArquivoSemExtensao = nome.Name.Substring(0, nome.Name.Count - (nome.Extension.Count))
+
+        Return _nomeDoArquivoSemExtensao
     End Function
 End Class

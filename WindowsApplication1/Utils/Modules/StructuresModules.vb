15e9c8f5d36e05c453793c9ea1602c411bbb6053
Fernando Rosa <fernandorosajr@gmail.com>
2020-02-17 17:44:56 -0300

    Criado Module StructuresModules com estrutura 'CaminhosDeRenomeDePastas' de dados para ser tratada pelo DialogForm 'FRMDialogConflictingFolders'.

---
+++ WindowsApplication1/Utils/Modules/StructuresModules.vb
@@ -0,0 +1,27 @@
+﻿Module StructuresModules
+
+    Public Structure CaminhosDeRenomeDePastas
+        Public oldPathOrigem As String
+        Public newPathOrigem As String
+        Public oldPathDestino As String
+        Public newPathDestino As String
+
+        Public oldPath As String
+        Public newPath As String
+    End Structure
+
+    Public _caminhosDasOperacoesComPastas As CaminhosDeRenomeDePastas
+
+    Public Structure employeem
+        Public givenName As String
+        Public familyName As String
+        Public phoneExtension As Long
+        Private salary As Decimal
+        Public Sub giveRaise(raise As Double)
+            salary *= raise
+        End Sub
+    End Structure
+
+    Public estrutura2 As employeem
+
+End Module

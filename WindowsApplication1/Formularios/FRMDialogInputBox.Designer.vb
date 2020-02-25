4ffc38d76337110f413f423556214c90b488f364
Fernando Rosa <fernandorosajr@gmail.com>
2020-02-25 14:24:47 -0300

    Alterado o layout e implementado eventos de Focus e Interação com o mouse em FRMDialogImputBox.

--- WindowsApplication1/Formularios/FRMDialogInputBox.Designer.vb
+++ WindowsApplication1/Formularios/FRMDialogInputBox.Designer.vb
@@ -29,8 +29,15 @@ Partial Class FRMDialogInputBox
         Me.TxtResponse = New System.Windows.Forms.TextBox()
         Me.LBLPrompt = New System.Windows.Forms.Label()
         Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LBLTitle01 = New System.Windows.Forms.Label()
+        Me.PanelBorder_TxtResponse = New System.Windows.Forms.Panel()
+        Me.PanelEnvolve_TxtResponse = New System.Windows.Forms.Panel()
         Me.TableLayoutPanel1.SuspendLayout()
         CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelBorder_TxtResponse.SuspendLayout()
+        Me.PanelEnvolve_TxtResponse.SuspendLayout()
         Me.SuspendLayout()
         '
         'TableLayoutPanel1
@@ -41,7 +48,7 @@ Partial Class FRMDialogInputBox
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(327, 102)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(365, 162)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 1
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
@@ -77,15 +84,14 @@ Partial Class FRMDialogInputBox
         '
         'TxtResponse
         '
-        Me.TxtResponse.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TxtResponse.BackColor = System.Drawing.Color.FromArgb(CType(CType(63, Byte), Integer), CType(CType(63, Byte), Integer), CType(CType(66, Byte), Integer))
-        Me.TxtResponse.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TxtResponse.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TxtResponse.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TxtResponse.ForeColor = System.Drawing.Color.DarkGray
-        Me.TxtResponse.Location = New System.Drawing.Point(12, 71)
+        Me.TxtResponse.Location = New System.Drawing.Point(3, 3)
         Me.TxtResponse.MaxLength = 32
         Me.TxtResponse.Name = "TxtResponse"
-        Me.TxtResponse.Size = New System.Drawing.Size(461, 20)
+        Me.TxtResponse.Size = New System.Drawing.Size(481, 13)
         Me.TxtResponse.TabIndex = 1
         '
         'LBLPrompt
@@ -94,15 +100,61 @@ Partial Class FRMDialogInputBox
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.LBLPrompt.ForeColor = System.Drawing.Color.DarkGray
-        Me.LBLPrompt.Location = New System.Drawing.Point(12, 13)
+        Me.LBLPrompt.Location = New System.Drawing.Point(130, 52)
         Me.LBLPrompt.Name = "LBLPrompt"
-        Me.LBLPrompt.Size = New System.Drawing.Size(461, 50)
+        Me.LBLPrompt.Size = New System.Drawing.Size(381, 51)
         Me.LBLPrompt.TabIndex = 2
         Me.LBLPrompt.Text = "Label1"
         '
         'ErrorProvider1
         '
         Me.ErrorProvider1.ContainerControl = Me
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.TrevoWebMedia.My.Resources.Resources.information__2_
+        Me.PictureBox1.Location = New System.Drawing.Point(22, 20)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(82, 84)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'LBLTitle01
+        '
+        Me.LBLTitle01.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LBLTitle01.AutoEllipsis = True
+        Me.LBLTitle01.Font = New System.Drawing.Font("Microsoft Tai Le", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LBLTitle01.ForeColor = System.Drawing.Color.LightGray
+        Me.LBLTitle01.Location = New System.Drawing.Point(126, 20)
+        Me.LBLTitle01.Name = "LBLTitle01"
+        Me.LBLTitle01.Size = New System.Drawing.Size(382, 32)
+        Me.LBLTitle01.TabIndex = 6
+        Me.LBLTitle01.Text = "Editar volume da unidade:"
+        '
+        'PanelBorder_TxtResponse
+        '
+        Me.PanelBorder_TxtResponse.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PanelBorder_TxtResponse.BackColor = System.Drawing.Color.FromArgb(CType(CType(83, Byte), Integer), CType(CType(83, Byte), Integer), CType(CType(86, Byte), Integer))
+        Me.PanelBorder_TxtResponse.Controls.Add(Me.PanelEnvolve_TxtResponse)
+        Me.PanelBorder_TxtResponse.Location = New System.Drawing.Point(22, 128)
+        Me.PanelBorder_TxtResponse.Name = "PanelBorder_TxtResponse"
+        Me.PanelBorder_TxtResponse.Padding = New System.Windows.Forms.Padding(1)
+        Me.PanelBorder_TxtResponse.Size = New System.Drawing.Size(489, 22)
+        Me.PanelBorder_TxtResponse.TabIndex = 7
+        '
+        'PanelEnvolve_TxtResponse
+        '
+        Me.PanelEnvolve_TxtResponse.BackColor = System.Drawing.Color.FromArgb(CType(CType(63, Byte), Integer), CType(CType(63, Byte), Integer), CType(CType(66, Byte), Integer))
+        Me.PanelEnvolve_TxtResponse.Controls.Add(Me.TxtResponse)
+        Me.PanelEnvolve_TxtResponse.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelEnvolve_TxtResponse.Location = New System.Drawing.Point(1, 1)
+        Me.PanelEnvolve_TxtResponse.Name = "PanelEnvolve_TxtResponse"
+        Me.PanelEnvolve_TxtResponse.Padding = New System.Windows.Forms.Padding(3)
+        Me.PanelEnvolve_TxtResponse.Size = New System.Drawing.Size(487, 20)
+        Me.PanelEnvolve_TxtResponse.TabIndex = 2
         '
         'FRMDialogInputBox
         '
@@ -111,9 +163,11 @@ Partial Class FRMDialogInputBox
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(63, Byte), Integer), CType(CType(63, Byte), Integer), CType(CType(66, Byte), Integer))
         Me.CancelButton = Me.Cancel_Button
-        Me.ClientSize = New System.Drawing.Size(485, 143)
+        Me.ClientSize = New System.Drawing.Size(528, 212)
+        Me.Controls.Add(Me.PanelBorder_TxtResponse)
+        Me.Controls.Add(Me.LBLTitle01)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.LBLPrompt)
-        Me.Controls.Add(Me.TxtResponse)
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.ForeColor = System.Drawing.Color.DarkGray
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
@@ -125,8 +179,11 @@ Partial Class FRMDialogInputBox
         Me.Text = "DialogRenameVolume"
         Me.TableLayoutPanel1.ResumeLayout(False)
         CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelBorder_TxtResponse.ResumeLayout(False)
+        Me.PanelEnvolve_TxtResponse.ResumeLayout(False)
+        Me.PanelEnvolve_TxtResponse.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
@@ -135,4 +192,8 @@ Partial Class FRMDialogInputBox
     Friend WithEvents TxtResponse As TextBox
     Friend WithEvents LBLPrompt As Label
     Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents LBLTitle01 As Label
+    Friend WithEvents PanelBorder_TxtResponse As Panel
+    Friend WithEvents PanelEnvolve_TxtResponse As Panel
 End Class
--- WindowsApplication1/Formularios/FRMDialogInputBox.vb
+++ WindowsApplication1/Formularios/FRMDialogInputBox.vb
@@ -1,6 +1,63 @@
 ﻿Imports System.Windows.Forms
 
 Public Class FRMDialogInputBox
+
+    ' Propriedades do configuração
+    Private _borderColorError As Color
+    Public Property BorderColorError As Color
+        Get
+            Return _borderColorError
+        End Get
+        Set(value As Color)
+            _borderColorError = value
+
+        End Set
+    End Property
+
+    Private _backColorCursoreGotoFocus As Color
+    Public Property BackColorCursoreGotoFocus As Color
+        Get
+            Return _backColorCursoreGotoFocus
+        End Get
+        Set(value As Color)
+            _backColorCursoreGotoFocus = value
+
+        End Set
+    End Property
+
+    Private _borderColorGotFocus As Color
+
+    Public Property BorderColorGotFocus As Color
+        Get
+            Return _borderColorGotFocus
+        End Get
+        Set(value As Color)
+            _borderColorGotFocus = value
+
+        End Set
+    End Property
+
+    Private _borderColorLostFocus As Color
+    Public Property BordeColorLostFocus As Color
+        Get
+            Return _borderColorLostFocus
+        End Get
+        Set(value As Color)
+            _borderColorLostFocus = value
+        End Set
+    End Property
+
+    Private _txtBoxBackColorLostFocus As Color
+
+    Public Property TXTBoxBackColorLostFocus As Color
+        Get
+            Return _txtBoxBackColorLostFocus
+        End Get
+        Set(value As Color)
+            _txtBoxBackColorLostFocus = value
+        End Set
+    End Property
+
     Dim _title As String
     Public Property Title As String
         Get
@@ -13,17 +70,6 @@ Public Class FRMDialogInputBox
         End Set
     End Property
 
-    Dim _defaultResponse As String
-    Public Property DefaultResponse
-        Get
-            Return _defaultResponse
-        End Get
-        Set(value)
-            _defaultResponse = value
-            TxtResponse.Text = value
-        End Set
-    End Property
-
     Dim _prompt As String
     Public Property Prompt As String
         Get
@@ -32,6 +78,17 @@ Public Class FRMDialogInputBox
         Set(value As String)
             _prompt = value
             LBLPrompt.Text = value
+        End Set
+    End Property
+
+    Dim _defaultResponse As String
+    Public Property DefaultResponse
+        Get
+            Return _defaultResponse
+        End Get
+        Set(value)
+            _defaultResponse = value
+            TxtResponse.Text = value
         End Set
     End Property
 
@@ -78,8 +135,108 @@ Public Class FRMDialogInputBox
     End Sub
 
     Private Sub TxtResponse_GotFocus(sender As Object, e As EventArgs) Handles TxtResponse.GotFocus
+        Dim txt As TextBox
+        txt = CType(sender, TextBox)
+        Dim _backColorMouseGotoFocus As Color = _backColorCursoreGotoFocus
+        Dim _corDaBorda_GotFocus As Color = _borderColorGotFocus
+        '  Dim borderColorError As Color = _borderColorError
+
+        'If Trim(txt.Text) = "" Or txt.Text.LastIndexOfAny(New Char() {"\"c, "/"c, "|"c, ":"c, "*"c, "?"c, """"c, "<"c, ">"c}) > -1 Then
+        '    txt.Parent.Parent.BackColor = borderColorError
+        'Else
+        '    txt.Parent.Parent.BackColor = backColor
+        'End If
+
+        txt.Parent.Parent.BackColor = _corDaBorda_GotFocus
+
+        txt.Cursor = Cursors.IBeam
+        txt.BackColor = _backColorMouseGotoFocus
+        txt.Parent.BackColor = _backColorMouseGotoFocus
+
         TxtResponse.SelectionStart = 0
         TxtResponse.SelectionLength = TxtResponse.Text.Length
     End Sub
 
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+        _borderColorGotFocus = SystemColors.HotTrack
+        _borderColorLostFocus = OK_Button.BackColor
+        _backColorCursoreGotoFocus = Color.FromArgb(45, 45, 48)
+        _borderColorError = Color.Red
+        _txtBoxBackColorLostFocus = Color.FromArgb(63, 63, 66)
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+    End Sub
+
+    Private Sub Buttons_LostFocus(sender As Object, e As EventArgs) Handles OK_Button.LostFocus, Cancel_Button.LostFocus
+        Dim btn As Button
+        btn = CType(sender, Button)
+
+        btn.FlatAppearance.BorderColor = _borderColorLostFocus
+    End Sub
+
+    Private Sub Buttons_GotFocus(sender As Object, e As EventArgs) Handles OK_Button.GotFocus, Cancel_Button.GotFocus
+        Dim btn As Button
+        btn = CType(sender, Button)
+
+        btn.FlatAppearance.BorderColor = _borderColorGotFocus
+
+    End Sub
+
+    Private Sub TxtResponse_MouseLeave(sender As Object, e As EventArgs) Handles TxtResponse.MouseLeave
+        Dim txt As TextBox
+        txt = CType(sender, TextBox)
+        Dim _corDaBorda_LostFocus As Color = _borderColorLostFocus
+        Dim _corDaBorda_GotFocus As Color = _borderColorGotFocus
+
+        If txt.Focused = True Then
+            ' txt.Parent.Parent.Padding = New Padding(0, 0, 0, 1)
+            txt.Parent.Parent.BackColor = _corDaBorda_GotFocus
+
+            'If Trim(txt.Text) = "" Or txt.Text.LastIndexOfAny(New Char() {"\"c, "/"c, "|"c, ":"c, "*"c, "?"c, """"c, "<"c, ">"c}) > -1 Then
+            '    txt.Parent.Parent.BackColor = BorderColorError
+            'Else
+            '    txt.Parent.Parent.BackColor = corGotFocus
+            'End If
+
+        Else
+            ' txt.Parent.Parent.Padding = New Padding(0, 0, 0, 1)
+            txt.Parent.Parent.BackColor = _corDaBorda_LostFocus
+        End If
+    End Sub
+
+    Private Sub TxtResponse_MouseMove(sender As Object, e As MouseEventArgs) Handles TxtResponse.MouseMove
+        Dim txt As TextBox
+        txt = CType(sender, TextBox)
+        Dim _corDaBorda_GotFocus As Color = _borderColorGotFocus
+        'Dim BorderColorError As Color = _borderColorError
+
+        txt.Parent.Parent.BackColor = _corDaBorda_GotFocus
+
+        'If Trim(txt.Text) = "" Or txt.Text.LastIndexOfAny(New Char() {"\"c, "/"c, "|"c, ":"c, "*"c, "?"c, """"c, "<"c, ">"c}) > -1 Then
+        '    txt.Parent.Parent.BackColor = BorderColorError
+        'Else
+        '    txt.Parent.Parent.BackColor = cor
+        'End If
+
+        'If txt.Focused = True Then
+        '    txt.Parent.Parent.Padding = New Padding(0, 0, 0, 1)
+        'Else
+        '    txt.Parent.Parent.Padding = New Padding(0, 0, 0, 1)
+        'End If
+    End Sub
+
+    Private Sub TxtResponse_LostFocus(sender As Object, e As EventArgs) Handles TxtResponse.LostFocus
+        Dim txt As TextBox
+        txt = CType(sender, TextBox)
+        Dim _corDeFundoDoTXTBoxLostFocus As Color = _txtBoxBackColorLostFocus
+
+        txt.Parent.Parent.BackColor = _borderColorLostFocus
+        '  txt.Parent.Parent.Padding = New Padding(0, 0, 0, 1)
+        txt.Cursor = Cursors.Arrow
+        txt.BackColor = _corDeFundoDoTXTBoxLostFocus
+        txt.Parent.BackColor = _corDeFundoDoTXTBoxLostFocus
+    End Sub
 End Class

522831f9da95262b8fa13abca99dd7651ed056d9
Fernando Rosa <fernandorosajr@gmail.com>
2020-05-03 19:18:42 -0300

    Criada a função ConverterParaListChar em StringFunctionClass

--- WindowsApplication1/Utils/Class/StringFunctionsClass.vb
+++ WindowsApplication1/Utils/Class/StringFunctionsClass.vb
@@ -7,6 +7,17 @@
 
     End Function
 
+    Function ConverterParaListChar(ByVal Quebra As String, ByVal delimitadores() As Char) As List(Of Char)
+        Dim palavras() As String
+        Dim caracteres As List(Of Char) = New List(Of Char)
+
+        palavras = SepararPalavras(Quebra, delimitadores)
+        '  caracteres =
+        For Each x As Char In palavras
+            caracteres.Add(x)
+        Next
+        Return caracteres
+    End Function
 
     Function ConverteStringEmColectionString(ByVal quebraPalavras As String, ByVal delimitatores() As Char) As Collections.Specialized.StringCollection
         Dim list As New Collections.Specialized.StringCollection

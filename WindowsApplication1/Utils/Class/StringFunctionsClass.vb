152c26b9c3ebad582db8fee4301a0af4c6a4c6c5
Fernando Rosa <fernandorosajr@gmail.com>
2020-05-14 13:29:13 -0300

    Adicionada as funções que faltavam em StringFunctionClass

--- WindowsApplication1/Utils/Class/StringFunctionsClass.vb
+++ WindowsApplication1/Utils/Class/StringFunctionsClass.vb
@@ -27,4 +27,78 @@
         Return list
     End Function
 
+    Function ConvertTheFirstWordToUppercase(textToConvert As String, firstLetterUpper As Boolean)
+        Dim newPhrase As String = ""
+        Dim charStr As String
+        Dim convertToUpper As Boolean
+        Dim fileName As String = textToConvert
+
+        convertToUpper = firstLetterUpper
+
+        For x = 0 To textToConvert.Length - 1
+
+            charStr = fileName.Substring(x, 1)
+
+            If convertToUpper = True Then
+                charStr = StrConv(charStr, vbUpperCase)
+                'convertToUpper = False
+
+            Else
+                charStr = StrConv(charStr, vbLowerCase)
+            End If
+
+            newPhrase += charStr
+
+            If fileName.Chars(x) = " " Then
+                convertToUpper = True
+
+            Else
+                convertToUpper = False
+
+            End If
+        Next
+
+        Return newPhrase
+    End Function
+
+    Function ConvertTheFirstLettersToLowercase(tEXTtOcONVERT As String)
+        Dim newPhrase As String = ""
+        Dim charStr As String
+        Dim convertToLower As Boolean
+        Dim fileName As String = tEXTtOcONVERT
+
+        convertToLower = True
+
+        For x = 0 To tEXTtOcONVERT.Length - 1
+
+            charStr = fileName.Substring(x, 1)
+
+            If convertToLower = True Then
+                charStr = StrConv(charStr, vbLowerCase)
+                'convertToUpper = False
+
+            Else
+                charStr = StrConv(charStr, vbUpperCase)
+            End If
+
+            newPhrase += charStr
+
+            If fileName.Chars(x) = " " Then
+                convertToLower = True
+
+            Else
+                convertToLower = False
+
+            End If
+        Next
+
+        Return newPhrase
+    End Function
+
+    Function ReplacePhrase(phrase As String, buscar As String, substituir As String)
+        Dim newPhrase As String
+        newPhrase = phrase.Replace(buscar, substituir)
+
+        Return newPhrase
+    End Function
 End Class

9e1b50f31dc0fe2565fc29dd625013ddf7178b53
Fernando Rosa <fernandorosajr@gmail.com>
2020-05-24 13:58:34 -0300

    Criada a função TextResult em Control_LoadRenameOption. Criada  a função AtualizarItensList em Control_LoadRenameOption. Criadas as propriedades FileExemple e Text.

--- WindowsApplication1/Formularios/FRMRenameCriterio/componentes/componentesDeFormularios/Control_LoadRenameOptions.vb
+++ WindowsApplication1/Formularios/FRMRenameCriterio/componentes/componentesDeFormularios/Control_LoadRenameOptions.vb
@@ -1,6 +1,19 @@
-﻿Public Class Control_LoadRenameOptions
+﻿Imports System.IO
+Public Class Control_LoadRenameOptions
     Dim CtlRenameOptionPrima As New Control_RenameOption
 
+    ' Propriedades
+    ' -------------------------------------------------------------
+    Private _text As String
+    Public Overrides Property Text As String
+        Get
+            Return _text
+        End Get
+        Set(value As String)
+            _text = value
+
+        End Set
+    End Property
 
     Private _itens As New List(Of Control)
     Public Property Itens As List(Of Control)
@@ -26,6 +39,33 @@
         End Set
     End Property
 
+    Private _fileExemple As FileInfo
+    Public Property FileExemple As String
+        Get
+            Return _fileExemple.FullName
+
+        End Get
+        Set(value As String)
+            If value IsNot Nothing Or Trim(value) = "" Then
+                _fileExemple = New FileInfo(value)
+
+            Else
+                value = "C:\Pasta Teste\Arquivo.txt"
+                _fileExemple = New FileInfo(value)
+
+            End If
+
+            ' TODO: Aqui entra uma função que...
+            '...adiciona as propriedades FileExemple dos subitens...
+
+            For Each item As Control_RenameOption In _itens
+                item.FileExemple = value
+            Next
+
+        End Set
+    End Property
+    ' -------------------------------------------------------------
+
     Private Sub Control_LoadRenameOptions_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -35,8 +75,12 @@
         ' Esta chamada é requerida pelo designer.
         InitializeComponent()
 
+        ' FileExemple = "C:\Pasta Teste\Meu ArquivO arQUiVado.txt"
+
         If _defaultItem Is Nothing Then
             _defaultItem = CtlRenameOptionPrima
+            ' TODO:  Ver se isso pode ficar no load
+            If _fileExemple IsNot Nothing Then CtlRenameOptionPrima.FileExemple = _fileExemple.FullName
         End If
 
         ' Adicione qualquer inicialização após a chamada InitializeComponent().
@@ -50,7 +94,59 @@
 
         End If
 
-
     End Sub
+
+    Sub AtualizarItensList()
+        Itens.Clear()
+        Itens.AddRange(Panel_LoadRenameOption.Controls.OfType(Of Control))
+    End Sub
+
+    Function TextResult() As String
+        Dim _textResult As String = ""
+        Dim item As Control_RenameOption
+        Dim temExtensao As Boolean = False
+        Dim _nomeDoArquivo As String = ""
+        Dim _extDoArquivo As String = ""
+
+        Dim x As Integer = 0
+        Dim y As Integer = _itens.Count - 1
+        Dim index As Integer = y - x
+
+        For x = 0 To y
+            index = y - x
+
+            item = _itens(index)
+
+#Disable Warning BC42025 ' Acesso do membro compartilhado, membro constante, membro enumerado ou tipo aninhado por meio de uma instância
+            If item.SelectDisplayTela = item._selectDisplayTela.NovaExtensao Then
+#Enable Warning BC42025 ' Acesso do membro compartilhado, membro constante, membro enumerado ou tipo aninhado por meio de uma instância
+                temExtensao = True
+
+            End If
+
+            If temExtensao = False Then
+                _nomeDoArquivo += item.Text
+            Else
+
+                _extDoArquivo += item.Text
+            End If
+
+        Next
+
+        If Trim(_nomeDoArquivo) = "" Then
+            _nomeDoArquivo = _fileExemple.Name.Replace(_fileExemple.Extension, "")
+
+        End If
+
+        If Trim(_extDoArquivo) = "" Or Trim(_extDoArquivo) = "." Then
+            _extDoArquivo = _fileExemple.Extension
+
+        End If
+
+        _textResult = _nomeDoArquivo + _extDoArquivo
+
+
+        Return _textResult
+    End Function
 
 End Class

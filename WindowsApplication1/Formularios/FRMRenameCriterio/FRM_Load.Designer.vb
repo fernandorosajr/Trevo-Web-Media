2637e43b92339028a182bec24bf3461bebf3625a
Fernando Rosa <fernandorosajr@gmail.com>
2020-06-17 11:53:21 -0300

    Altedado o design de FRM_Load.

--- WindowsApplication1/Formularios/FRMRenameCriterio/FRM_Load.Designer.vb
+++ WindowsApplication1/Formularios/FRMRenameCriterio/FRM_Load.Designer.vb
@@ -23,33 +23,104 @@ Partial Class FRM_Load
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.PanelBorder = New System.Windows.Forms.Panel()
+        Me.PanelBackground = New System.Windows.Forms.Panel()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Cancel_Button = New System.Windows.Forms.Button()
+        Me.PanelBorder.SuspendLayout()
+        Me.PanelBackground.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'ProgressBar1
         '
         Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.ProgressBar1.Location = New System.Drawing.Point(29, 72)
+        Me.ProgressBar1.Location = New System.Drawing.Point(30, 50)
         Me.ProgressBar1.Name = "ProgressBar1"
         Me.ProgressBar1.Size = New System.Drawing.Size(527, 23)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
         Me.ProgressBar1.TabIndex = 0
+        '
+        'PanelBorder
+        '
+        Me.PanelBorder.BackColor = System.Drawing.Color.FromArgb(CType(CType(83, Byte), Integer), CType(CType(83, Byte), Integer), CType(CType(86, Byte), Integer))
+        Me.PanelBorder.Controls.Add(Me.ProgressBar1)
+        Me.PanelBorder.Controls.Add(Me.PanelBackground)
+        Me.PanelBorder.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelBorder.Location = New System.Drawing.Point(0, 0)
+        Me.PanelBorder.Name = "PanelBorder"
+        Me.PanelBorder.Padding = New System.Windows.Forms.Padding(1)
+        Me.PanelBorder.Size = New System.Drawing.Size(585, 144)
+        Me.PanelBorder.TabIndex = 1
+        '
+        'PanelBackground
+        '
+        Me.PanelBackground.BackColor = System.Drawing.Color.FromArgb(CType(CType(63, Byte), Integer), CType(CType(63, Byte), Integer), CType(CType(66, Byte), Integer))
+        Me.PanelBackground.Controls.Add(Me.TableLayoutPanel1)
+        Me.PanelBackground.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelBackground.Location = New System.Drawing.Point(1, 1)
+        Me.PanelBackground.Name = "PanelBackground"
+        Me.PanelBackground.Size = New System.Drawing.Size(583, 142)
+        Me.PanelBackground.TabIndex = 0
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 0, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(29, 95)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(527, 29)
+        Me.TableLayoutPanel1.TabIndex = 5
+        Me.TableLayoutPanel1.Visible = False
+        '
+        'Cancel_Button
+        '
+        Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Cancel_Button.BackColor = System.Drawing.Color.FromArgb(CType(CType(63, Byte), Integer), CType(CType(63, Byte), Integer), CType(CType(66, Byte), Integer))
+        Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Abort
+        Me.Cancel_Button.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(83, Byte), Integer), CType(CType(83, Byte), Integer), CType(CType(86, Byte), Integer))
+        Me.Cancel_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Cancel_Button.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Cancel_Button.ForeColor = System.Drawing.Color.DarkGray
+        Me.Cancel_Button.Location = New System.Drawing.Point(230, 3)
+        Me.Cancel_Button.Name = "Cancel_Button"
+        Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
+        Me.Cancel_Button.TabIndex = 0
+        Me.Cancel_Button.Text = "Cancelar"
+        Me.Cancel_Button.UseVisualStyleBackColor = True
         '
         'FRM_Load
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(63, Byte), Integer), CType(CType(63, Byte), Integer), CType(CType(66, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(585, 166)
-        Me.Controls.Add(Me.ProgressBar1)
+        Me.ClientSize = New System.Drawing.Size(585, 144)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.PanelBorder)
         Me.ForeColor = System.Drawing.Color.LightGray
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "FRM_Load"
+        Me.ShowIcon = False
         Me.ShowInTaskbar = False
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
-        Me.Text = "FRM_Load"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Carregando..."
+        Me.PanelBorder.ResumeLayout(False)
+        Me.PanelBackground.ResumeLayout(False)
+        Me.TableLayoutPanel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents PanelBorder As Panel
+    Friend WithEvents PanelBackground As Panel
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Cancel_Button As Button
 End Class
--- WindowsApplication1/Formularios/FRMRenameCriterio/FRM_Load.vb
+++ WindowsApplication1/Formularios/FRMRenameCriterio/FRM_Load.vb
@@ -1,3 +1,9 @@
 ﻿Public Class FRM_Load
 
+
+    Private Sub FRM_Load_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
 End Class

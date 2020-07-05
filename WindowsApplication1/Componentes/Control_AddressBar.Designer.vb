9bc8146ad9e99cac395af1b43256169edc001c38
Fernando Rosa <fernandorosajr@gmail.com>
2020-07-05 11:58:27 -0300

    Adicionado Control_AdderssBar.

---
+++ WindowsApplication1/Componentes/Control_AddressBar.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Control_AddressBar
+    Inherits System.Windows.Forms.UserControl
+
+    'O UserControl substitui o descarte para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Control_AddressBar))
+        Me.Panel9 = New System.Windows.Forms.Panel()
+        Me.Panel35 = New System.Windows.Forms.Panel()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Panel36 = New System.Windows.Forms.Panel()
+        Me.Panel37 = New System.Windows.Forms.Panel()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Panel9.SuspendLayout()
+        Me.Panel35.SuspendLayout()
+        Me.Panel36.SuspendLayout()
+        Me.Panel37.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel9
+        '
+        Me.Panel9.BackColor = System.Drawing.Color.Gainsboro
+        Me.Panel9.Controls.Add(Me.Panel35)
+        Me.Panel9.Controls.Add(Me.Panel36)
+        Me.Panel9.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel9.Location = New System.Drawing.Point(0, 0)
+        Me.Panel9.Name = "Panel9"
+        Me.Panel9.Padding = New System.Windows.Forms.Padding(1)
+        Me.Panel9.Size = New System.Drawing.Size(677, 76)
+        Me.Panel9.TabIndex = 18
+        '
+        'Panel35
+        '
+        Me.Panel35.BackColor = System.Drawing.Color.White
+        Me.Panel35.Controls.Add(Me.TextBox3)
+        Me.Panel35.Controls.Add(Me.Button17)
+        Me.Panel35.Controls.Add(Me.Button16)
+        Me.Panel35.Controls.Add(Me.Button15)
+        Me.Panel35.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel35.Location = New System.Drawing.Point(1, 1)
+        Me.Panel35.Name = "Panel35"
+        Me.Panel35.Padding = New System.Windows.Forms.Padding(3)
+        Me.Panel35.Size = New System.Drawing.Size(647, 74)
+        Me.Panel35.TabIndex = 8
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox3.ForeColor = System.Drawing.Color.DimGray
+        Me.TextBox3.Location = New System.Drawing.Point(33, 3)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(596, 13)
+        Me.TextBox3.TabIndex = 9
+        Me.TextBox3.Text = "Pesquisar"
+        '
+        'Button17
+        '
+        Me.Button17.BackColor = System.Drawing.Color.White
+        Me.Button17.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button17.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button17.FlatAppearance.BorderSize = 0
+        Me.Button17.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Button17.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button17.Image = CType(resources.GetObject("Button17.Image"), System.Drawing.Image)
+        Me.Button17.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Button17.Location = New System.Drawing.Point(629, 3)
+        Me.Button17.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(15, 68)
+        Me.Button17.TabIndex = 18
+        Me.Button17.UseVisualStyleBackColor = False
+        '
+        'Button16
+        '
+        Me.Button16.BackColor = System.Drawing.Color.White
+        Me.Button16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button16.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Button16.FlatAppearance.BorderSize = 0
+        Me.Button16.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lavender
+        Me.Button16.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button16.Image = CType(resources.GetObject("Button16.Image"), System.Drawing.Image)
+        Me.Button16.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Button16.Location = New System.Drawing.Point(18, 3)
+        Me.Button16.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(15, 68)
+        Me.Button16.TabIndex = 16
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'Button15
+        '
+        Me.Button15.BackColor = System.Drawing.Color.White
+        Me.Button15.BackgroundImage = Global.TrevoWebMedia.My.Resources.Resources.pasta_1
+        Me.Button15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button15.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Button15.FlatAppearance.BorderSize = 0
+        Me.Button15.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lavender
+        Me.Button15.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button15.ImageKey = "(nenhum/a)"
+        Me.Button15.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Button15.Location = New System.Drawing.Point(3, 3)
+        Me.Button15.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(15, 68)
+        Me.Button15.TabIndex = 15
+        Me.Button15.UseVisualStyleBackColor = False
+        '
+        'Panel36
+        '
+        Me.Panel36.BackColor = System.Drawing.Color.Gainsboro
+        Me.Panel36.Controls.Add(Me.Panel37)
+        Me.Panel36.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel36.Location = New System.Drawing.Point(648, 1)
+        Me.Panel36.Name = "Panel36"
+        Me.Panel36.Padding = New System.Windows.Forms.Padding(1, 0, 0, 0)
+        Me.Panel36.Size = New System.Drawing.Size(28, 74)
+        Me.Panel36.TabIndex = 21
+        '
+        'Panel37
+        '
+        Me.Panel37.BackColor = System.Drawing.Color.White
+        Me.Panel37.Controls.Add(Me.Button18)
+        Me.Panel37.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel37.Location = New System.Drawing.Point(1, 0)
+        Me.Panel37.Name = "Panel37"
+        Me.Panel37.Padding = New System.Windows.Forms.Padding(3)
+        Me.Panel37.Size = New System.Drawing.Size(27, 74)
+        Me.Panel37.TabIndex = 8
+        '
+        'Button18
+        '
+        Me.Button18.BackColor = System.Drawing.Color.White
+        Me.Button18.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button18.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button18.FlatAppearance.BorderSize = 0
+        Me.Button18.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lavender
+        Me.Button18.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button18.Image = CType(resources.GetObject("Button18.Image"), System.Drawing.Image)
+        Me.Button18.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Button18.Location = New System.Drawing.Point(3, 3)
+        Me.Button18.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(21, 68)
+        Me.Button18.TabIndex = 17
+        Me.Button18.UseVisualStyleBackColor = False
+        '
+        'Control_AddressBar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Panel9)
+        Me.Name = "Control_AddressBar"
+        Me.Size = New System.Drawing.Size(677, 76)
+        Me.Panel9.ResumeLayout(False)
+        Me.Panel35.ResumeLayout(False)
+        Me.Panel35.PerformLayout()
+        Me.Panel36.ResumeLayout(False)
+        Me.Panel37.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents Panel35 As Panel
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Panel36 As Panel
+    Friend WithEvents Panel37 As Panel
+    Friend WithEvents Button18 As Button
+End Class
---
+++ WindowsApplication1/Componentes/Control_AddressBar.vb
@@ -0,0 +1,3 @@
+﻿Public Class Control_AddressBar
+
+End Class

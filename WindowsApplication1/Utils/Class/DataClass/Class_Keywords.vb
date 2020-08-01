5cdba2d2b939d2edd2f0cd8499ec90a4fd6bdb76
Fernando Rosa <fernandorosajr@gmail.com>
2020-08-01 17:46:48 -0300

    Adicionado Class_Keywords e Movido Class_DataRenamingCriterion

---
+++ WindowsApplication1/Utils/Class/DataClass/Class_Keywords.vb
@@ -0,0 +1,59 @@
+﻿Public Class Class_Keywords
+
+    Private _teste As String
+    Public Property Teste As String
+        Get
+            Return _teste
+
+        End Get
+        Set(value As String)
+            _teste = value
+        End Set
+    End Property
+
+
+    Public _nodeAssociated As List(Of TreeNode)
+    Public Property NodeAssociated() As List(Of TreeNode)
+        Get
+
+            Return _nodeAssociated
+        End Get
+        Set(value As List(Of TreeNode))
+
+            _nodeAssociated = value
+
+        End Set
+
+    End Property
+
+    Public teste2 As Integer
+    'Public key As KeywordNode
+
+    'Public Structure KeywordNode
+
+    '    Private _keyword As String
+    '    Public Property Keyword As String
+    '        Get
+    '            Return _keyword
+    '        End Get
+    '        Set(value As String)
+    '            _keyword = value
+    '        End Set
+    '    End Property
+
+    '    Private _nodeAssociated As List(Of TreeNode)
+    '    Public Property NodeAssociated() As List(Of TreeNode)
+    '        Get
+
+    '            Return _nodeAssociated
+    '        End Get
+    '        Set(value As List(Of TreeNode))
+
+    '            _nodeAssociated = value
+
+    '        End Set
+
+    '    End Property
+    'End Structure
+
+End Class
